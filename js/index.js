88c779c799ec019232a46ce08de3ccb83bd3f9ed
FUSU123fusu <88240205+FUSU123fusu@users.noreply.github.com>
2023-01-07 09:41:14 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value;
+    var pwd=document.getElementById("pwd").value;
+		//修改密码请改此处
+    if(userName=="崔祖娇" &&  pwd=="1226"){
+			event.preventDefault();
+			$('form').fadeOut(500);
+			$('.wrapper').addClass('form-success');
+			setTimeout(function(){location.href="BirthdayCake.html";},2000);
+		}
+	else{
+		alert("密码是你的生日哦");
+	}
+});
